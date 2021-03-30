04237e71ddf6da67819a4d265a0e94e8293552fa
YosukeSawanoTU <sawano-yosuke@edu.k.u-tokyo.ac.jp>
2021-03-30 16:58:07 +0900

    change effective r

--- Model/RotBondedInteraction.cpp
+++ Model/RotBondedInteraction.cpp
@@ -260,8 +260,8 @@ CRotBondedInteraction::CRotBondedInteraction(CRotParticle* p1,CRotParticle* p2,c
 
   // sawano ////////////
   // m_normalthreshold = effA * param.truncated;
-  // m_normalthreshold = M_PI * Rave * Rave * param.truncated;
-  m_normalthreshold = M_PI * effA * param.truncated;
+  m_normalthreshold = M_PI * Rave * Rave * param.truncated;
+  // m_normalthreshold = M_PI * effA * param.truncated;
   m_isFirst = true;
   //////////////////////
 }
